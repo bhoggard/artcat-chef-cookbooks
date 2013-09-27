4d80596ea88da899c0a0d88048b26a04e95baa70
Barry Hoggard <barry@tristanmedia.com>
2013-09-27 18:57:10 -0400

    fixes made for Chef 11 on opsworks

--- opsworks_custom_env/metadata.rb
+++ opsworks_custom_env/metadata.rb
@@ -3,3 +3,5 @@ description "Writes a config/application.yml file with custom ENV values to apps
 version "0.1"
 
 recipe "opsworks_custom_env::configure", "Write a config/application.yml file to app's deploy directory. Relies on restart command declared by rails::configure recipe. (Intended as part of configure/deploy OpsWorks events.)"
+
+depends "rails"
