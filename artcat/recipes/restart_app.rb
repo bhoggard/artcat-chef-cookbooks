947db69be372bbdb26f59c2a25effcadf0bb65a0
Barry Hoggard <barry@tristanmedia.com>
2013-12-28 18:50:11 -0500

    added startup of papertrail remote_syslog to rails restart

--- artcat/recipes/restart_app.rb
+++ artcat/recipes/restart_app.rb
@@ -4,6 +4,9 @@ node[:deploy].each do |application, deploy|
   execute "restart Rails app #{application}" do
     cwd deploy[:current_path]
     command "#{deploy[:deploy_to]}/shared/scripts/unicorn restart"
+
+    # make sure remote_syslog for papertrail is running
+    command "/usr/local/bin/bundle exec #{node[:deploy][application][:rake]} papertrail:start"
     action :nothing
   end
 
